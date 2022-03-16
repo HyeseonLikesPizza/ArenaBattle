30b711abd33a4a6dcbe5d301a5a5756545e17c69
HyeSeon <1725f0701@gmail.com>
2022-03-16 20:30:28 +0900

    ch2

--- Source/ArenaBattle/Fountain.cpp
+++ Source/ArenaBattle/Fountain.cpp
@@ -9,12 +9,52 @@ AFountain::AFountain()
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	// 컴포넌트 생성
+
 	Body = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("BODY"));
 	Water = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("WATER"));
+	Light = CreateDefaultSubobject<UPointLightComponent>(TEXT("LIGHT"));
+	Splash = CreateDefaultSubobject<UParticleSystemComponent>(TEXT("SPLASH"));
+
+	// Body를 루트 컴포넌트로 만들고, 나머지 컴포넌트들을 Body의 자식 컴포넌트로 붙인다.
 
 	RootComponent = Body;
 	Water->SetupAttachment(Body);
+	Light->SetupAttachment(Body);
+	Splash->SetupAttachment(Body);
 
+	// 컴포넌트들의 상대 위치를 설정한다.
+
+	Water->SetRelativeLocation(FVector(0, 0, 135.0f));
+	Light->SetRelativeLocation(FVector(0.0f, 0.0f, 195.0f));
+	Splash->SetRelativeLocation(FVector(0.0f, 0.0f, 195.0f));
+
+	// 스태틱메시 애셋 포인터 객체를 생성하고 Body의 스태틱 메시를 해당 애셋으로 설정한다.
+	// 애셋의 경로 정보는 게임 실행 중에 변경될 일이 없기 때문에 애셋 포인터는 static으로 선언해서 한 번만 초기화해주는 것이 좋다!!
+
+	static ConstructorHelpers::FObjectFinder<UStaticMesh> 
+		SM_BODY(TEXT("/Game/InfinityBladeGrassLands/Environments/Plains/Env_Plains_Ruins/StaticMesh/SM_Plains_Castle_Fountain_01.SM_Plains_Castle_Fountain_01"));
+
+	if (SM_BODY.Succeeded())
+	{
+		Body->SetStaticMesh(SM_BODY.Object);
+	}
+	
+	static ConstructorHelpers::FObjectFinder<UStaticMesh>
+		SM_WATER(TEXT("/Game/InfinityBladeGrassLands/Effects/FX_Meshes/Env/SM_Plains_Fountain_02.SM_Plains_Fountain_02"));
+
+	if (SM_WATER.Succeeded())
+	{
+		Water->SetStaticMesh(SM_WATER.Object);
+	}
+
+	static ConstructorHelpers::FObjectFinder<UParticleSystem>
+		PS_SPALSH(TEXT("/Game/InfinityBladeGrassLands/Effects/FX_Ambient/Water/P_Water_Fountain_Splash_Base_01.P_Water_Fountain_Splash_Base_01"));
+
+	if (PS_SPALSH.Succeeded())
+	{
+		Splash->SetTemplate(PS_SPALSH.Object);
+	}
 }
 
 // Called when the game starts or when spawned
