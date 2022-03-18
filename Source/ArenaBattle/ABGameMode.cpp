d67113b1eb3cc4089c863adf49157abbd9a1fa28
HyeSeon <1725f0701@gmail.com>
2022-03-18 22:54:30 +0900

    0318 ch5

--- Source/ArenaBattle/ABGameMode.cpp
+++ Source/ArenaBattle/ABGameMode.cpp
@@ -2,11 +2,25 @@
 
 #include "ABGameMode.h"
 #include "ABPawn.h"
+#include "ABPlayerController.h"
 
 
 AABGameMode::AABGameMode() {
 
 	DefaultPawnClass = AABPawn::StaticClass();
-
+	PlayerControllerClass = AABPlayerController::StaticClass();
+	
+	// 블루프린트 애셋을 C++에 가져오는 방법
+	/*
+	static ConstructorHelpers::FClassFinder<APawn> BP_PAWN_C(TEXT("/Game/ThirdPersonBP/Blueprints/ThirdPersonCharacter.ThirdPersonCharacter_C"));
+	if (BP_PAWN_C.Succeeded())
+	{
+		DefaultPawnClass = BP_PAWN_C.Class;
+	}
+	*/
 }
 
+void AABGameMode::PostLogin(APlayerController * NewPlayer)
+{
+	Super::PostLogin(NewPlayer);
+}
---
+++ Source/ArenaBattle/ABPlayerController.cpp
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "ABPlayerController.h"
+
+void AABPlayerController::PostInitializeComponents()
+{
+	Super::PostInitializeComponents();
+
+}
+
+void AABPlayerController::Possess(APawn * aPawn)
+{
+
+	Super::Possess(aPawn);
+}
+
