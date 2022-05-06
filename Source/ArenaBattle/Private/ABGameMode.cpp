f17a000c8df9bc35d1af55f45348945f282a60e3
HyeSeon <1725f0701@gmail.com>
2022-05-06 13:49:14 +0900

    0506 ch14

---
+++ Source/ArenaBattle/Private/ABGameMode.cpp
@@ -0,0 +1,60 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "ABGameMode.h"
+#include "ABCharacter.h"
+#include "ABPlayerController.h"
+#include "ABPlayerState.h"
+#include "ABGameState.h"
+
+
+AABGameMode::AABGameMode() {
+
+	DefaultPawnClass = AABCharacter::StaticClass();
+	PlayerControllerClass = AABPlayerController::StaticClass();
+	PlayerStateClass = AABPlayerState::StaticClass();
+	GameStateClass = AABGameState::StaticClass();
+	
+	// 블루프린트 애셋을 C++에 가져오는 방법
+	/*
+	static ConstructorHelpers::FClassFinder<APawn> BP_PAWN_C(TEXT("/Game/ThirdPersonBP/Blueprints/ThirdPersonCharacter.ThirdPersonCharacter_C"));
+	if (BP_PAWN_C.Succeeded())
+	{
+		DefaultPawnClass = BP_PAWN_C.Class;
+	}
+	*/
+}
+
+void AABGameMode::PostInitializeComponents()
+{
+	Super::PostInitializeComponents();
+	ABGameState = Cast<AABGameState>(GameState);
+}
+
+void AABGameMode::PostLogin(APlayerController * NewPlayer)
+{
+	Super::PostLogin(NewPlayer);
+
+	auto ABPlayerState = Cast<AABPlayerState>(NewPlayer->PlayerState);
+	ABCHECK(nullptr != ABPlayerState);
+	ABPlayerState->InitPlayerData();
+}
+
+void AABGameMode::AddScore(AABPlayerController * ScoredPlayer)
+{
+	for (FConstPlayerControllerIterator It = GetWorld()->GetPlayerControllerIterator(); It; ++It)
+	{
+		const auto ABPlayerController = Cast<AABPlayerController>(It->Get());
+		if ((nullptr != ABPlayerController) && (ScoredPlayer == ABPlayerController))
+		{
+			ABPlayerController->AddGameScore();
+			break;
+		}
+	}
+
+	ABGameState->AddGameScore();
+}
+
+int32 AABGameMode::GetScore() const
+{
+	return ABGameState->GetTotalGameScore();
+}
